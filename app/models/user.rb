739c9fb3e149c44f5f5379cc5fe408a052a02887
Dinh Xuan Son <dinh.xuan.son@framgia.com>
2015-10-29 10:44:34 +0700

    devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable , :trackable, :validatable, ,
+  devise :database_authenticatable, :registerable, :recoverable,
+          :rememberable
   has_many :user_subjects
   has_many :user_tasks
   has_many :activities
@@ -9,25 +12,4 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-  def remember
-    self.remember_token = User.new_token
-    update_attributes remember_digest: User.digest(remember_token)
-  end
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-  def forget
-    update_attributes remember_digest: nil
-  end
 end
